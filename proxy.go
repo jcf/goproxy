f4c17d906e538e149d32504495ace3d3bd154a4e
James Conroy-Finn <james@logi.cl>
2013-05-12 18:57:46 +0100

    Remove commented-out code

--- proxy.go
+++ proxy.go
@@ -9,13 +9,6 @@ import (
   "time"
 )
 
-/* func benchmark(description string, f func()) interface{} { */
-/*   t := time.Now() */
-/*   f() */
-/*   delta := time.Since(t) */
-/*   log.Printf("%s took %v", description, delta) */
-/* } */
-
 func fetchRemote(url string) (*http.Response, error) {
   client := &http.Client{
   /* CheckRedirect: redirectPolicyFunc, */
@@ -39,7 +32,6 @@ func readResponse(resp *http.Response) ([]byte, error) {
 // TODO DRY
 func handleRequest(w http.ResponseWriter, r *http.Request) {
   url := html.EscapeString(r.FormValue("url"))
-  // log.Printf("Proxying %s\n", url)
 
   t := time.Now()
   resp, err := fetchRemote(url)
@@ -61,13 +53,7 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-  // resp, err := http.Get("http://example.com/")
-
   http.HandleFunc("/", handleRequest)
-
-  // http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-  //   fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-  // })
 
   log.Printf("Starting up... http://localhost:8080")
   log.Fatal(http.ListenAndServe(":8080", nil))
